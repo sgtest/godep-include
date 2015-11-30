eaf371f28d3eb3f0841c5a41bcad166243e463b7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-11-30 17:39:51 +0200

    initial commit

---
+++ Godeps/_workspace/src/github.com/sourcegraph/john-test/hi/hi.go
@@ -0,0 +1,6 @@
+package hi
+
+// GetHi returns a string saying hi
+func GetHi() string {
+	return "Hello World"
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sourcegraph/john-test/hi"
+)
+
+func main() {
+	fmt.Println(hi.GetHi())
+}
